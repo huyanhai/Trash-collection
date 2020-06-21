00d54f12477567d5c9386944d032031d46357013
huyanhai <810839700@qq.com>
2020-06-21 18:48:01 +0800

    文件

---
+++ pages/complate.vue
@@ -0,0 +1,151 @@
+<template>
+	<view class="question-content">
+		<image src="../static/img/question-bg.png" class="bg" mode=""></image>
+		<view class="box">
+			<image src="../static/img/complatebg.png" mode="" class="c-bg"></image>
+			<view class="complate-info">
+				<text class="title">恭喜您！</text>
+				<text class="score">100%</text>
+			</view>
+			<view class="details">
+				<view class="item">
+					<view class="col-l">错题数</view>
+					<view class="col-r yellow">0</view>
+				</view>
+				<view class="item">
+					<view class="col-l">本次答题用时</view>
+					<view class="col-r green">00:02:00</view>
+				</view>
+				<view class="item">
+					<view class="col-l">积分奖励</view>
+					<view class="col-r green">1</view>
+				</view>
+			</view>
+			<view class="tips">今日答题次数已用完，明天记得再来哦~</view>
+		</view>
+	</view>
+</template>
+
+<script>
+import { get, post } from '../libs/request.js';
+import subject from "../components/subject.vue"
+export default {
+	data() {
+		return {
+			answer:["易腐垃圾","其他垃圾","可回收物","有害垃圾"]
+			
+		};
+	},
+	onLoad(e) {
+		
+	},
+	onShow(){
+		
+	},
+	onHide(){
+		
+	},
+	components: {
+		subject
+	},
+	methods: {
+		getData(data){
+			post("/goods/query",data).then(res=>{
+				console.log(res)
+			})
+		},
+		goPage(path){
+			uni.navigateTo({
+				url: path
+			})
+		}
+	}
+};
+</script>
+
+<style lang="scss">
+.question-content{
+	background: #FFFFFF;
+	box-sizing: border-box;
+	width: 100%;
+	min-height: 100vh;
+	position: relative;
+	overflow: hidden;
+	.bg{
+		position: absolute;
+		top: 0rpx;
+		left: 0;
+		width: 750rpx;
+		height: 257rpx;
+	}
+	.box{
+		width: 650rpx;
+		height: 980rpx;
+		border-radius: 20rpx;
+		background: #FFFFFF;
+		margin: 60rpx auto 0 auto;
+		position: relative;
+		z-index: 10;
+		box-shadow: 0 0 10rpx rgba($color: #000000, $alpha: 0.1);
+		overflow: hidden;
+		.c-bg{
+			height: 325rpx;
+			width: 650rpx;
+			position: absolute;
+			top: 0;
+			left: 0;
+		}
+		.complate-info{
+			width: 100%;
+			text-align: center;
+			position: relative;
+			z-index: 10;
+			margin-top: 40rpx;
+			.title{
+				font-size: 36rpx;
+				color: #FFFFFF;
+				display: block;
+				margin-bottom: 90rpx;
+			}
+			.score{
+				font-size: 36rpx;
+				color: #FFFFFF;
+				display: block;
+			}
+		}
+		.details{
+			position: relative;
+			z-index: 10;
+			margin-top: 140rpx;
+			padding: 0 60rpx;
+			.item{
+				display: flex;
+				justify-content: space-between;
+				line-height: 60rpx;
+				height: 60rpx;
+				margin-bottom: 20rpx;
+				.col-l{
+					font-size: 30rpx;
+					color: #666666;
+				}
+				.col-r{
+					&.yellow{
+						color: #D79814;
+					}
+					&.green{
+						color: #2B8349;
+					}
+				}
+			}
+		}
+		.tips{
+			position: absolute;
+			width: 100%;
+			bottom: 120rpx;
+			font-size: 28rpx;
+			color: #D79814;
+			text-align: center;
+		}
+	}
+}
+</style>
